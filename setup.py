a45b22db40d54ccfa5e5b244569fb8eb56ef3719
nixon <nixon@shun.org>
2015-07-08 17:47:07 -0500

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
         name="gstat",
         author="nixon",
         url="https://github.com/nixon/gstat/",
-        version="0.1.0",
+        version="0.1.1",
         license="LICENSE",
         description='Send metrics to a graphite host.',
         long_description=open('README.md').read(),
